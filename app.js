ef397251dbb58ac4b4e874cbf0766445e10297ca
Orca <Orcade@mail.com>
2024-02-15 15:01:11 -0400

    change port

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ db.once('open',()=>{
     console.log("Database connected");
 });
 
-
-app.listen(3000,()=>{
-    console.log('Servering on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log('Servering on port ',port)
 })
 
 app.engine('ejs',ejsMate);
